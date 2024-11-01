2578fa4638c8c6cd118889e81ad289c303e444fd
zggff <gigamaximwachau@gmail.com>
2024-11-01 14:28:49 +0300

    switch to c++

--- lab02/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// variant 9
-
-int main(void) {
-    printf("init\n");
-    return 0;
-}
---
+++ lab02/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// variant 9 Рассчитать детерминант матрицы (используя определение детерминанта)
+
+int main(void) {
+    std::cout << "hello" << std::endl;
+    return 0;
+}
