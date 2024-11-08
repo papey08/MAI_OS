0f27b1e05d661c8003874cfa801a3a3b591e0a4a
Dmitriy <marindmitriy996@gmail.com>
2024-11-08 10:06:15 +0300

    delete stdio.h and fix rand

--- lab02/src/main.c
+++ lab02/src/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <pthread.h>
 #include <time.h>
-#include <stdio.h>
+
+#define MAX_THREADS 12
 
 int validate_nums(int amount, char* nums[]) {
     for (int j = 0; j <= amount; ++j) {
@@ -17,35 +18,102 @@ int validate_nums(int amount, char* nums[]) {
     return 1;
 }
 
-void* calc_score(void* arg) {
-    int rounds = *(int*)arg;
-    int* score = malloc(sizeof(int) * 2);
-    for (int i = 0; i < rounds; i++)
-    {
-        score[0] += rand() % 6 + 1;
-        score[0] += rand() % 6 + 1;
-
-        score[1] += rand() % 6 + 1;
-        score[1] += rand() % 6 + 1;
-
+void float_to_string(long double value, char *buffer, int len, int precision) {
+    for (int i = 0; i < len; ++i) {
+        buffer[i] = '\0';
     }
-    *(int*)arg = score[0];
-    free(arg);
-    return score;
+    if (value < 0) {
+        *buffer++ = '-';
+        value = -value; 
+    }
+    int integerPart = (int)value;
+    float fractionalPart = value - integerPart;
+
+    char *intPtr = buffer;
+    if (integerPart == 0) {
+        *intPtr++ = '0'; 
+    } else {
+        char temp[20]; 
+        int i = 0;
+        while (integerPart > 0) {
+            temp[i++] = (integerPart % 10) + '0'; 
+            integerPart /= 10; 
+        }
+        while (i > 0) {
+            *intPtr++ = temp[--i];
+        }
+    }
+
+    *intPtr++ = '.';
+
+    for (int i = 0; i < precision; i++) {
+        fractionalPart *= 10; 
+        int fractionalDigit = (int)fractionalPart; 
+        *intPtr++ = fractionalDigit + '0'; 
+        fractionalPart -= fractionalDigit; 
+    }
+
+    *intPtr++ = '%';
+    *intPtr = '\n';
 }
 
-// ввод (5 цифорок):
-// количество бросков двух костей, какой сейчас тур, сколько очков суммарно у каждого из игроков и
-// количество экспериментов, которые должна произвести программа
+void* calc_score(void* arg) {
+    // проблема в том, что использование rand() не позволяет достичь требуемой производительности,
+    // поэтому используем свой генератор псевдослучайных чисел
+
+    unsigned long current  = time(NULL);
+    const unsigned long a = 1664525;       
+    const unsigned long c = 1013904223;    
+    const unsigned long m = 4294967296;    
+
+
+    int rounds = ((int*)arg)[0];
+    int exp = ((int*)arg)[1];
+    int first_score = ((int*)arg)[2];
+    int second_score = ((int*)arg)[3];
+    int* score = malloc(sizeof(int) * 2);
+    long double* result = malloc(sizeof(long double) * 3);
+    for (int j = 0; j < exp; j ++) {
+        score[0] = (float)first_score;
+        score[1] = (float)second_score;
+        for (int i = 0; i < rounds; i++) {
+            current = (current * a + c) % m;
+            score[0] += current % 6 + 1;
+            current = (current * a + c) % m;
+            score[0] += current % 6 + 1;
+
+            current = (current * a + c) % m;
+            score[1] += current % 6 + 1;
+            current = (current * a + c) % m;
+            score[1] += current % 6 + 1;
+        }
+
+        if(score[0] > score[1]) {
+            result[0] += 1.;
+            result[2] += 1.;
+        } else if(score[0] < score[1]) {
+            result[1] += 1.;
+            result[2] += 1.;
+        }
+    }
+    free(arg);
+    free(score);
+    return result;
+}
+
+// ввод (6 цифорок):
+// количество бросков двух костей, какой сейчас тур, сколько очков суммарно у каждого из игроков,
+// количество экспериментов, которые должна произвести программа, количество потоков
 int main(int argc, char* argv[]) {
-    srand(time(NULL));
-    if (argc != 6) {
-        printf("USAGE: ./a.out <total rounds> <current tour> <first_score> <second_score> <experiments>\n");
+    if (argc != 7) { 
+        char msg[] = "USAGE: ./a.out <total rounds> <current tour> <first_score> <second_score> <experiments> <threads>\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return 1;
     }
 
     if (validate_nums(argc, argv)) {
-        printf("ERROR: all input numbers must be integer and positive\n");
+        char msg[] = "ERROR: all input numbers must be integer and positive\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return 2;
     }
 
@@ -54,61 +122,88 @@ int main(int argc, char* argv[]) {
     int first_score = atoi(argv[3]);
     int second_score = atoi(argv[4]);
     int experiments = atoi(argv[5]);
+    int num_threads = atoi(argv[6]);
+
 
     if (k < tour) {
-        printf("ERROR: total rounds must be greater than current tour\n");
+        char msg[] = "ERROR: total rounds must be greater than current tour\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return 3;
+    } 
+
+    if (num_threads > MAX_THREADS || num_threads < 1) {
+        char msg[] = "ERROR: number of threads must be lesser than 12 and greater than 1\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        return 4;
     }
 
     // если игра уже окончена, то у одного шанс на победу 100%, а у второго 0%
     if (k == tour) {
         if (first_score > second_score) {
-            printf("First player win with 100%% probability\n");
-            printf("Second player win with 0%% probability\n");
+            char msg1[] = "First player win with 100%% probability\n";
+            char msg2[] = "Second player win with 0%% probability\n";
+            write(STDOUT_FILENO, msg1, sizeof(msg1) - 1);
+            write(STDOUT_FILENO, msg2, sizeof(msg2) - 1);
         } else if (first_score < second_score) {
-            printf("First player win with 0%% probability\n");
-            printf("Second player win with 100%% probability\n");
+            char msg1[] = "First player win with 0%% probability\n";
+            char msg2[] = "Second player win with 100%% probability\n";
+            write(STDOUT_FILENO, msg1, sizeof(msg1) - 1);
+            write(STDOUT_FILENO, msg2, sizeof(msg2) - 1);
         } else {
-            printf("First player win with 100%% probability\n");
-            printf("Second player win with 100%% probability\n");
+            char msg1[] = "First player win with 100%% probability\n";
+            char msg2[] = "Second player win with 100%% probability\n";
+            write(STDOUT_FILENO, msg1, sizeof(msg1) - 1);
+            write(STDOUT_FILENO, msg2, sizeof(msg2) - 1);
         }
         return 0;
     }
 
-    pthread_t experiments_threads[experiments];
 
-    for (int i = 0; i < experiments; ++i) {
-        int* rounds = malloc(sizeof(int));
-        *rounds = k - tour;
-        if (pthread_create(&experiments_threads[i], NULL, &calc_score, rounds)) {
-            printf("ERROR: thread cannot be created\n");
-            return 4;
-        }
-    }
+    pthread_t experiments_threads[num_threads];
 
-    float first_prob = 0.;
-    float second_prob = 0.;
-    float exp = (float)experiments;
-
-    for (int i = 0; i < experiments; ++i) {
-        int* scores;
-        if (pthread_join(experiments_threads[i], (void**)&scores)) {
-            printf("ERROR: thread cannot be joined\n");
+    for (int i = 0; i < num_threads; ++i) {
+        int* data_for_calc = malloc(sizeof(int) * 4);
+        data_for_calc[0] = k - tour;
+        data_for_calc[1] = experiments / num_threads + 1;
+        data_for_calc[2] = first_score;
+        data_for_calc[3] = second_score;
+        if (pthread_create(&experiments_threads[i], NULL, calc_score, data_for_calc)) {
+            char msg[] = "ERROR: thread cannot be created\n";
+            write(STDERR_FILENO, msg, sizeof(msg) - 1);
             return 5;
         }
-        if ((scores[0] + first_score) > (scores[1] + second_score)) {
-            first_prob += 1;
-        } else if ((scores[0] + first_score) < (scores[1] + second_score)) {
-            second_prob += 1;
-        } else {
-            exp--;
-        }
-        // вывести очки по итогу одного эксперимента
-        // printf("%d %d\n", scores[0], scores[1]);
     }
-    first_prob = first_prob / (float)exp * 100;
-    second_prob =  second_prob / (float)exp * 100;
-    printf("First player win with %.2f%% probability\n", first_prob);
-    printf("Second player win with %.2f%% probability\n", second_prob);
+
+    long double first_prob = 0.;
+    long double second_prob = 0.;
+    long double exp = 0.;
+
+    for (int i = 0; i < num_threads; ++i) {
+        long double* scores;
+        if (pthread_join(experiments_threads[i], (void**)&scores)) {
+            char msg[] = "ERROR: thread cannot be joined\n";
+            write(STDERR_FILENO, msg, sizeof(msg) - 1);
+            return 6;
+        }
+        first_prob += scores[0];
+        second_prob += scores[1];
+        exp += scores[2];
+    }
+
+    first_prob = first_prob / exp * 100;
+    second_prob = second_prob / exp * 100;
+
+    char num[16];
+    float_to_string(first_prob, num, 16, 2);
+
+    char msg1[] = "Probability of the first player winnig - ";
+    write(STDOUT_FILENO, msg1, sizeof(msg1) - 1);
+    write(STDOUT_FILENO, num, sizeof(num) - 1);
+
+    float_to_string(second_prob, num, 16, 2);
+    
+    char msg2[] = "Probability of the second player winnig - ";
+    write(STDOUT_FILENO, msg2, sizeof(msg2) - 1);
+    write(STDOUT_FILENO, num, sizeof(num) - 1);
     return 0;
 }
