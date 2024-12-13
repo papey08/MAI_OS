b5c6d1bc3c94d7cb7b0dabb2566fd1d9cd0e6e0a
AdamArselgov <adamarselgov4@gmail.com>
2024-12-13 22:32:28 +0300

    убрал stdio.h

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,19 +1,41 @@
-#include <stdio.h>
 #include <dlfcn.h>
 #include <sys/mman.h>
+#include <unistd.h>  
+#include <string.h>  
+
 #include "allocator.h"
 
 #define MEMORY_SIZE 1024 * 1024
 
+void my_write(const char* message) {
+    write(STDERR_FILENO, message, strlen(message));
+}
+
+
+void my_write_hex(void* ptr) {
+    char buffer[64];
+    unsigned long addr = (unsigned long)ptr;
+    size_t i; 
+    for (i = 0; i < sizeof(buffer) - 1 && addr; ++i) {
+        unsigned char byte = addr & 0xF;
+        buffer[i] = (byte < 10) ? '0' + byte : 'a' + (byte - 10);
+        addr >>= 4;
+    }
+    buffer[i] = '\0'; 
+    my_write(buffer);
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <path_to_allocator_library>\n", argv[0]);
+        my_write("Usage: <path_to_allocator_library>\n");
         return 1;
     }
 
     void* handle = dlopen(argv[1], RTLD_LAZY);
     if (!handle) {
-        fprintf(stderr, "Failed to load library: %s\n", dlerror());
+        my_write("Failed to load library: ");
+        my_write(dlerror());
+        my_write("\n");
         return 1;
     }
 
@@ -24,21 +46,23 @@ int main(int argc, char* argv[]) {
 
     char* error;
     if ((error = dlerror()) != NULL) {
-        fprintf(stderr, "Error resolving symbols: %s\n", error);
+        my_write("Error resolving symbols: ");
+        my_write(error);
+        my_write("\n");
         dlclose(handle);
         return 1;
     }
 
     void* memory = mmap(NULL, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (memory == MAP_FAILED) {
-        perror("mmap failed");
+        my_write("mmap failed\n");
         dlclose(handle);
         return 1;
     }
 
     Allocator* allocator = allocator_create(memory, MEMORY_SIZE);
     if (!allocator) {
-        fprintf(stderr, "Failed to create allocator\n");
+        my_write("Failed to create allocator\n");
         munmap(memory, MEMORY_SIZE);
         dlclose(handle);
         return 1;
@@ -46,13 +70,15 @@ int main(int argc, char* argv[]) {
 
     void* block = allocator_alloc(allocator, 128);
     if (block) {
-        printf("Allocated block at %p\n", block);
+        my_write("Allocated block at ");
+        my_write_hex(block);
+        my_write("\n");
     } else {
-        printf("Failed to allocate block\n");
+        my_write("Failed to allocate block\n");
     }
 
     allocator_free(allocator, block);
-    printf("Freed block at %p\n", block);
+    my_write("Freed block\n");
 
     allocator_destroy(allocator);
     munmap(memory, MEMORY_SIZE);
